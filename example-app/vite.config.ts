ff26d6368c63432dee57d8cac9ff5ba869f4c20e
vdalseng <vdalseng@gmail.com>
2025-01-14 16:05:32 +0100

    Add vite config and enable build script

---
+++ example-app/vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  root: './', // Ensure this points to the directory containing index.html
+  build: {
+    outDir: 'dist',
+  },
+});
